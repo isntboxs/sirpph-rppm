22b7d14568dc8bb13c671d0c691b448f2e412a25
MrBoxs <rizkywanda200155@gmail.com>
2026-04-04 22:34:22 +0700

    setup eslint, prettier, react compiler and update dependencies

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,20 +1,40 @@
-//  @ts-check
+// @ts-nocheck
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
 
-export default [
+export default defineConfig([
   ...tanstackConfig,
-  {
-    rules: {
-      'import/no-cycle': 'off',
-      'import/order': 'off',
-      'sort-imports': 'off',
-      '@typescript-eslint/array-type': 'off',
-      '@typescript-eslint/require-await': 'off',
-      'pnpm/json-enforce-catalog': 'off',
-    },
-  },
+  reactHooks.configs.flat.recommended,
   {
     ignores: ['eslint.config.js', 'prettier.config.js'],
   },
-]
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: [
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    rules: {
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
+      '@typescript-eslint/only-throw-error': 'off',
+    },
+  },
+])
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,11 @@ const config = defineConfig({
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
-    viteReact(),
+    viteReact({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
   ],
 })
 
