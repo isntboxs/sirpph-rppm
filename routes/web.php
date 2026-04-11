8cda1288e783099696c87cf81f8c4b6cfb99b63a
XevenTH <emmanuelangga@gmail.com>
2026-04-11 21:33:01 +0700

    update: redirect to

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/login', function () {
     return view('auth.login');
-});
+})->name('login');
