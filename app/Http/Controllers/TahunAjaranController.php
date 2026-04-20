91b5875219cc63ac3af0773a59c895b96c9b999d
XevenTH <emmanuelangga@gmail.com>
2026-04-20 23:29:22 +0700

    add: tahun ajaran controller

--- app/Http/Controllers/TahunAjaranController.php
+++ app/Http/Controllers/TahunAjaranController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\TahunAjaran;
+use Illuminate\Http\Request;
 
 class TahunAjaranController extends Controller
 {
     public function index()
     {
-        return view('pages.tahun_ajaran.index');
+        $datas = TahunAjaran::select(['id', 'name', 'active', 'semester'])
+            ->orderByDesc('active')
+            ->get();
+
+        return view('pages.tahun_ajaran.index', compact('datas'));
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'name'     => 'required|string|max:20',
+            'semester' => 'required|integer|in:1,2',
+        ]);
+
+        TahunAjaran::create([
+            'name'     => $request->name,
+            'active'    => false,
+            'semester' => $request->semester,
+        ]);
+
+        return response()->json(['msg' => 'Tahun Ajaran berhasil ditambahkan']);
+    }
+
+    public function active($id)
+    {
+        TahunAjaran::query()->update(['active' => false]);
+
+        TahunAjaran::findOrFail($id)->update(['active' => true]);
+
+        return response()->json(['msg' => 'Tahun Ajaran berhasil diaktifkan']);
     }
 }
